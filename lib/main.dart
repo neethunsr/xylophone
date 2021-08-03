ce4e1bb7f9ae742137a54c7651b90f70d2d8c907
neethunsr <neethu2001nsr@gmail.com>
2021-08-03 23:47:48 +0530

    added audio package

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,35 @@
 import 'package:flutter/material.dart';
+import 'package:audioplayers/audioplayers.dart';
 
 void main() => runApp(XylophoneApp());
 
 class XylophoneApp extends StatelessWidget {
+  void playSound(int note) {
+    final player = AudioCache();
+    player.play('note$note.wav');
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         body: SafeArea(
-          child: Container(),
+          child: Column(
+            children: [
+              Expanded(
+                child: TextButton(
+                  style: TextButton.styleFrom(primary: Colors.purple),
+                  // style: ButtonStyle(
+                  //   backgroundColor: MaterialStateProperty.all(Colors.purple),
+                  // ),
+                  onPressed: () {
+                    playSound(1);
+                  },
+                  child: null,
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
